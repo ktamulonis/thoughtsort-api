9f62d1dd6ec68cca4c71de6c530323470cefc47f
Kurt Tamulonis <kurttamulonis@gmail.com>
2017-12-03 19:44:50 -0500

    added update action to ideas controller

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -4,5 +4,10 @@ module Api::V1
       @ideas = Idea.order("created_at DESC")
       render json: @ideas
     end
+    def  update
+      @idea = Idea.find(params[:id])
+      @idea.update_attributes(idea_params)
+      render json: @idea
+    end
   end
 end
