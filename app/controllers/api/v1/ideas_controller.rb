7f013beeaefe20cae47b99104695b60dace73294
Kurt Tamulonis <kurttamulonis@gmail.com>
2017-12-03 20:12:27 -0500

    added crud

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -9,5 +9,23 @@ module Api::V1
       @idea.update_attributes(idea_params)
       render json: @idea
     end
+    def create
+      @idea = Idea.create(idea_params)
+      render json: @idea
+    end
+    def destroy
+      @idea = Idea.find(params[:id])
+      if @idea.destroy
+        head :no_content, status: :ok
+      else
+        render json: @idea.errors, status: :unprocessable_entity
+      end
+    end   
+  private
+
+    def idea_params
+      params.require(:idea).permit(:title, :body)
+    end
+
   end
 end
